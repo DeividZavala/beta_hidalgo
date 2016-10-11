27ece7816c0929f8aef647666075d5eb6babefe0
DeividZavala <crusedmoss@gmail.com>
2016-10-10 19:08:10 -0500

    Service

--- app/components/user_profile/user_profile.js
+++ app/components/user_profile/user_profile.js
@@ -1,16 +1,25 @@
 (function () {
 
-    var userProfile = {
+    var user = {
         templateUrl:'app/components/user_profile/user_profile.html',
         controller:profileController
     }
 
-    function profileController() {
+    profileController.$inject = ['$routeParams','hidalgoService'];
+    function profileController($routeParams,hidalgoService) {
         let profile = this;
 
         profile.panelSetTab = panelSetTab;
         profile.checkTab = checkTab;
         profile.tab = 1;
+
+        profile.userId = $routeParams.id;
+        console.log(profile.userId)
+
+        hidalgoService.userDetail(profile.userId).then(function (response) {
+            profile.userProfile = response.data[0].fields
+            console.log(profile.userProfile)
+        })
 
         function panelSetTab(setTab) {
             profile.tab = setTab;
@@ -23,5 +32,5 @@
 
     angular
         .module('hidalgo')
-        .component('userProfileComponent',userProfile);
+        .component('userProfileComponent',user);
 })();
