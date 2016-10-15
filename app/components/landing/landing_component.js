7337c54b0aa73395fc9da71cf291ad3af2b693fb
BlisS <contacto@fixter.org>
2016-10-15 09:19:19 -0500

    slider

--- app/components/landing/landing_component.js
+++ app/components/landing/landing_component.js
@@ -3,7 +3,25 @@
 
 	var landing ={
 
-		templateUrl : 'app/components/landing/landing.html'
+		templateUrl : 'app/components/landing/landing.html',
+		controller:landingController
+	}
+
+	function landingController($scope){
+
+		
+		  $(".owl-carousel").owlCarousel({
+		  	navigation:false,
+		  	slideSpeed:300,
+		  	paginationSpeed:400,
+		  	singleItem:true,
+		  	autoPlay:true,
+		  	stopOnHover:false
+		  });
+		
+
+
+
 	}
 
 	angular
