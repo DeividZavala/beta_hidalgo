80f06c978dbcd2469f093579f3c5053d29dad5fb
BlisS <contacto@fixter.org>
2016-10-18 12:38:26 -0500

    experimentos csrf

--- app/app.js
+++ app/app.js
@@ -1,4 +1,34 @@
 (function () {
     angular
-        .module('hidalgo',['ngRoute','firebase']);
+        .module('hidalgo',['ngRoute','firebase','ngCookies'])
+        .config(configura);
+        
+        function configura($httpProvider){
+
+            $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+            $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+        }
+
+
+// config.$inject = ['$httpProvider']
+// config.$inject = ['$httpProvider','$cookies'];
+
+    // function configuracion($httpProvider){
+ //    function configuracion($routeProvider,$httpProvider){
+    	
+    	
+
+ //    	// $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+ //    	// $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+	// 	// $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+ //        // $httpProvider.defaults.withCredentials = false;
+        
+ //        // $httpProvider.defaults.headers.post['X-CSRFToken'] = $cookies.get('csrftoken');
+
+	// // var token = $('input[name=csrfmiddlewaretoken]').val();
+ // //    $httpProvider.defaults.headers.post['X-CSRFToken'] = token;
+
+ //    }
+
+
 })();
