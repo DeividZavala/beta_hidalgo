991e75d5481dd994073c08c530132248f9f3cb93
DeividZavala <crusedmoss@gmail.com>
2016-10-28 09:50:42 -0500

    permisos de completar proyecto

--- app/components/project_details/project_details.js
+++ app/components/project_details/project_details.js
@@ -114,9 +114,10 @@
           self.user = firebaseUser;
 
           if(self.user){
-
+              //console.log(self.user.uid)
              // self.alert = "Bienvenido "+self.user.displayName;
             // self.cuentale()
+            self.uid = self.user.uid
 
           }else{
 
@@ -133,16 +134,20 @@
        // hidalgoService.getProjectDetail(projectDetails.id)
        $http.get('http://planestataldedesarrollo.hidalgo.gob.mx:8000/projects/'+$routeParams.id+'/')
             .then(function (response) {
-                projectDetails.data = response.data[0]
-                //console.log(projectDetails.data)
-               // console.log(projectDetails.data.fields.title)
-                //console.log("la imagen",projectDetails.data.fields.imagen)
-                $scope.proyecto = response.data[0].fields
-                $scope.proyecto.pk = response.data[0].pk
-                $scope.pro = $scope.proyecto
-                $scope.barra();
+                self.project_uid = response.data[0].fields.uid
+                if(self.project_uid === self.uid){
+                    projectDetails.data = response.data[0]
+                    //console.log(projectDetails.data.fields.uid)
+                    // console.log(projectDetails.data.fields.title)
+                    //console.log("la imagen",projectDetails.data.fields.imagen)
+                    $scope.proyecto = response.data[0].fields
+                    $scope.proyecto.pk = response.data[0].pk
+                    $scope.pro = $scope.proyecto
+                    $scope.barra();
+                }else{
+                    $location.path('/')
+                }
                 
-
             });
 
 
